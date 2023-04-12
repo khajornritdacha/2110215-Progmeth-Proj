06bb627177ac2a03a908ebba142bf242c841138f
vijak khajornritdacha <vijak13019@hotmail.com>
2023-04-12 12:00:33 +0700

    feat(scene): add initial select roles scene

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,13 +4,15 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import scene.chooseRole.ChooseRolePane;
 import scene.startGame.StartGameRootPane;
 
 public class Main extends Application {
 	
     @Override
     public void start(Stage stage) {
-    	Pane root = new StartGameRootPane();
+//    	Pane root = new StartGameRootPane();
+    	Pane root = new ChooseRolePane();
     	Scene scene = new Scene(root, 800, 640);
     	stage.setTitle("RPG Life");
     	stage.setScene(scene);
