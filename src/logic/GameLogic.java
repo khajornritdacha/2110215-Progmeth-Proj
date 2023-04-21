a7c507ce368e0e8044590096a01f87a6236b9f0e
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-04-21 13:53:55 +0700

    fix: the game will end when the number of player less then or equal to 1 (not 0)

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -218,7 +218,7 @@ public class GameLogic {
 	}
 	
 	public void goToNextPlayer() {
-		if (this.getNumberAlivePlayers() == 0) {
+		if (this.getNumberAlivePlayers() <= 1) {
 			endGame();
 			return;
 		}
