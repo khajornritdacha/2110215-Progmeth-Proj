46ccb8e4ff06eb43770e93c330101aecc7be80ae
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-05-20 02:33:05 +0700

    Remove docs

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"action"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"customException"},{"l":"logic"},{"l":"monster"},{"l":"player"},{"l":"scene"},{"l":"scene.components"},{"l":"scene.InputPlayerName"},{"l":"utility"}];updateSearchResults();
